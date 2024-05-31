972d778c45d75676d9a40fa97589dd1365f0b05b
frostedoyster <bigi.f@libero.it>
2024-05-31 07:25:30 +0200

    Provide more thorough testing against ASE

---
+++ python/vesin/tests/data/readme.txt
@@ -0,0 +1,7 @@
+This is a small test suite for vesin, composed of:
+- water.xyz: a medium-sized cluster
+- napthalene.xyz: a small organic molecule
+- diamond.xyz: a periodic system with a cubic cell
+- carbon.xyz: a periodic system with a non-cubic cell
+
+TODO: add systems with boundary conditions along some directions, but not others
--- python/vesin/tests/test_neighbors.py
+++ python/vesin/tests/test_neighbors.py
@@ -1,28 +1,57 @@
-import ase
+import os
+
 import ase.build
+import ase.io
 import ase.neighborlist
 import numpy as np
+import pytest
 
 import vesin
 
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-def test_neighbors():
-    atoms = ase.build.make_supercell(ase.build.bulk("Si", "diamond"), 3 * np.eye(3))
 
-    ase_nl = ase.neighborlist.neighbor_list("ijSD", atoms, cutoff=3.5)
-    ase_nl = [(i, j, S, D) for i, j, S, D in zip(*ase_nl)]
+@pytest.mark.parametrize("system", ["water", "diamond", "naphthalene", "carbon"])
+@pytest.mark.parametrize("quantities", ["ijSD", "ijS"])
+@pytest.mark.parametrize("cutoff", [float(i) for i in range(1, 10)])
+def test_neighbors(system, quantities, cutoff):
+    atoms = ase.io.read(f"{CURRENT_DIR}/data/{system}.xyz")
 
-    vesin_nl = vesin.ase_neighbor_list("ijSD", atoms, cutoff=3.5)
-    vesin_nl = [(i, j, S, D) for i, j, S, D in zip(*vesin_nl)]
+    ase_nl = ase.neighborlist.neighbor_list(quantities, atoms, cutoff)
+    if quantities == "ijS":
+        ase_nl = [(i, j, S) for i, j, S in zip(*ase_nl)]
+    elif quantities == "ijSD":
+        ase_nl = [(i, j, S, D) for i, j, S, D in zip(*ase_nl)]
+    else:
+        raise ValueError(f"Unknown quantities: {quantities}")
+
+    vesin_nl = vesin.ase_neighbor_list(quantities, atoms, cutoff)
+    if quantities == "ijS":
+        vesin_nl = [(i, j, S) for i, j, S in zip(*vesin_nl)]
+    elif quantities == "ijSD":
+        vesin_nl = [(i, j, S, D) for i, j, S, D in zip(*vesin_nl)]
 
     assert len(ase_nl) == len(vesin_nl)
 
-    for i, j, S, D in vesin_nl:
+    for sample in vesin_nl:
+        if quantities == "ijS":
+            i, j, S = sample
+        elif quantities == "ijSD":
+            i, j, S, D = sample
+        else:
+            raise ValueError(f"Unknown quantities: {quantities}")
         found = False
-        for ref_i, ref_j, ref_S, ref_D in ase_nl:
+        for ref_sample in ase_nl:
+            if quantities == "ijS":
+                ref_i, ref_j, ref_S = ref_sample
+            elif quantities == "ijSD":
+                ref_i, ref_j, ref_S, ref_D = ref_sample
+            else:
+                raise ValueError(f"Unknown quantities: {quantities}")
             if i == ref_i and j == ref_j and np.all(S == ref_S):
-                assert np.allclose(D, ref_D)
                 found = True
+                if "D" in quantities:
+                    assert np.allclose(D, ref_D)
                 break
 
         assert found

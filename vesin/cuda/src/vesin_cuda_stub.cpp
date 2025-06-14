325179b60e51631410b8233023e65716a6a4f01f
Nick J. Browning <nickjbrowning@gmail.com>
2025-06-14 02:05:37 +0200

    updated stub

--- vesin/cuda/src/vesin_cuda_stub.cpp
+++ vesin/cuda/src/vesin_cuda_stub.cpp
@@ -8,14 +8,10 @@ void free_neighbors(VesinNeighborList & neighbors) {
     throw std::runtime_error("vesin was not compiled with CUDA support");
 }
 
-void ensure_neighborlist_capacity(VesinNeighborList & neighbors, size_t required_capacity) {
-    throw std::runtime_error("vesin was not compiled with CUDA support");
-}
-
 void neighbors(
-    const double (*points)[3],          // [n_points][3] on device
+    const double (*points)[3],
     size_t n_points,
-    const double cell[3][3],            // [3][3] on device
+    const double cell[3][3],
     VesinOptions options,
     VesinNeighborList& neighbors
 ) {

7d04fb66cd7f060e23c76ddf9265f68490d126a5
mgoonde <miha.gunde@gmail.com>
2025-07-09 09:56:31 +0200

    add README in test

---
+++ fortran/test/README.md
@@ -0,0 +1,15 @@
+# Description
+
+test the fortran wrapper to `vesin`.
+
+# Compile
+
+```bash
+sh compile.sh
+```
+
+# Run
+
+```bash
+./main.x
+```
--- fortran/test/compile.sh
+++ fortran/test/compile.sh
@@ -1,3 +1,4 @@
+if [ ! -e ../../vesin-single-build.cpp ]; then cd ../../ && python3 create-single-cpp.py && cd -; fi
 g++ -I../../vesin/include -J./ -c ../../vesin-single-build.cpp
 gfortran -c ../f_vesin_wrapper.f90
 gfortran -I./ -o main.x main.f90 vesin-single-build.o f_vesin_wrapper.o -lstdc++

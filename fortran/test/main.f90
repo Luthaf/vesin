70e28d771984567a5dc5beba44734b14e000db99
mgoonde <miha.gunde@gmail.com>
2025-07-09 14:47:05 +0200

    change names in test

--- fortran/test/main.f90
+++ fortran/test/main.f90
@@ -1,23 +1,24 @@
 program main
-  use f_vesin_wrapper, only: vesin_t, rp
+  use vesin, only: vesin_t
+  use, intrinsic :: iso_c_binding
   implicit none
   type( vesin_t ), pointer :: neigh
-  integer, parameter :: nat=2
-  real(rp) :: pos(3,nat)
-  real(rp) :: box(3,3)
-  integer :: ierr
+  integer( c_size_t ), parameter :: nat=2
+  real(c_double) :: pos(3,nat)
+  real(c_double) :: box(3,3)
+  integer(c_int) :: ierr
 
   ! positions
-  pos(:,1) = [ 0.0_rp, 0.0_rp, 0.0_rp ]
-  pos(:,2) = [ 0.0_rp, 1.3_rp, 1.3_rp ]
+  pos(:,1) = [ 0.0_c_double, 0.0_c_double, 0.0_c_double ]
+  pos(:,2) = [ 0.0_c_double, 1.3_c_double, 1.3_c_double ]
 
   ! lattice
-  box(:,1) = [ 3.2_rp, 0.0_rp, 0.0_rp ]
-  box(:,2) = [ 0.0_rp, 3.2_rp, 0.0_rp ]
-  box(:,3) = [ 0.0_rp, 0.0_rp, 3.2_rp ]
+  box(:,1) = [ 3.2_c_double, 0.0_c_double, 0.0_c_double ]
+  box(:,2) = [ 0.0_c_double, 3.2_c_double, 0.0_c_double ]
+  box(:,3) = [ 0.0_c_double, 0.0_c_double, 3.2_c_double ]
 
   ! create the instance, set options
-  neigh => vesin_t( cutoff=4.2_rp, full=.true., return_shifts=.true., return_distances=.true. )
+  neigh => vesin_t( cutoff=4.2_c_double, full=.true., return_shifts=.true., return_distances=.true. )
 
   ! launch computation of neighbor list
   ierr = neigh% compute( nat, pos, box )

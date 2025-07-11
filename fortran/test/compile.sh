7b5aff84d68f74aba17967cdd48171bfdf4a902d
Guillaume Fraux <guillaume.fraux@epfl.ch>
2025-07-11 16:22:11 +0200

    Integrate the fortran code with cmake

--- fortran/test/compile.sh
+++
@@ -1,4 +0,0 @@
-if [ ! -e ../../vesin-single-build.cpp ]; then cd ../../ && python3 create-single-cpp.py && cd -; fi
-g++ -I../../vesin/include -J./ -c ../../vesin-single-build.cpp
-gfortran -g -c ../vesin.f90
-gfortran -g -I./ -o main.x main.f90 vesin-single-build.o vesin.o -lstdc++

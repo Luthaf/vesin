4bf078f4b543570a5f2ce3c349f63fc294e877a9
Nick J. Browning <nickjbrowning@gmail.com>
2025-07-31 14:37:55 +0200

    formatting.

--- vesin/src/mic_neighbourlist.cu
+++ vesin/src/mic_neighbourlist.cu
@@ -1,10 +1,9 @@
 #include "mic_neighbourlist.cuh"
 
-#include <stdexcept>
 #include <assert.h>
 #include <cstdio>
 #include <cuda_runtime.h>
-
+#include <stdexcept>
 
 #define NWARPS 4
 #define WARP_SIZE 32

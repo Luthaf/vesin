d91b8f33c689e873c37cf5bf646e2215b540b5ad
Guillaume Fraux <guillaume.fraux@epfl.ch>
2026-05-08 17:40:48 +0200

    Add a version of the single file build without cuda
    The cuda code adds 8k lines which are not needed in most
    cases where someone would use the single file build

--- vesin/scripts/create-single-cpp.py
+++ vesin/scripts/create-single-cpp.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 import os
+import shutil
 import subprocess
 import tarfile
 import tempfile
 
 
 HERE = os.path.dirname(os.path.realpath(__file__))
+ROOT = os.path.realpath(os.path.join(HERE, "..", ".."))
+DIST = os.path.realpath(os.path.join(HERE, "..", "dist"))
 ALREADY_SEEN = set()
 
 
 def find_file(path):
     candidates = [
-        os.path.join(HERE, "vesin", "src", path),
-        os.path.join(HERE, "vesin", "include", path),
+        os.path.join(ROOT, "vesin", "src", path),
+        os.path.join(ROOT, "vesin", "include", path),
         os.path.join(os.getcwd(), path),
         os.path.join(os.getcwd(), "_deps", "gpulite-src", path),
     ]
@@ -64,7 +67,7 @@ def merge_files(path, output):
 
 
 def add_version(output):
-    with open(os.path.join(HERE, "vesin", "VERSION")) as fd:
+    with open(os.path.join(ROOT, "vesin", "VERSION")) as fd:
         version = fd.read().strip()
 
     output.write("// automatically generated \n")
@@ -72,13 +75,13 @@ def add_version(output):
 
 
 if __name__ == "__main__":
-    with open(os.path.join(HERE, "vesin", "VERSION")) as fd:
+    with open(os.path.join(ROOT, "vesin", "VERSION")) as fd:
         version = fd.read().strip()
 
     with tempfile.TemporaryDirectory() as tempdir:
         os.chdir(tempdir)
         subprocess.run(
-            ["cmake", os.path.join(HERE, "vesin")],
+            ["cmake", os.path.join(ROOT, "vesin")],
             check=True,
         )
 
@@ -87,23 +90,40 @@ if __name__ == "__main__":
             check=True,
         )
 
-        with open(os.path.join(HERE, "vesin-single-build.cpp"), "w") as output:
+        os.makedirs(DIST, exist_ok=True)
+
+        ALREADY_SEEN.clear()
+        with open(os.path.join(DIST, "vesin-single-build.cpp"), "w") as output:
             add_version(output)
             merge_files("cpu_cell_list.cpp", output)
             merge_files("vesin_cuda.cpp", output)
             merge_files("vesin.cpp", output)
 
-        tarpath = os.path.join(HERE, f"vesin-single-build-v{version}.tar.gz")
+        ALREADY_SEEN.clear()
+        with open(os.path.join(DIST, "vesin-single-build-nocuda.cpp"), "w") as output:
+            add_version(output)
+            merge_files("cpu_cell_list.cpp", output)
+            merge_files("vesin_cuda_stub.cpp", output)
+            merge_files("vesin.cpp", output)
+
+        shutil.copyfile(
+            os.path.join(ROOT, "vesin", "include", "vesin.h"),
+            os.path.join(DIST, "vesin.h"),
+        )
+
+        tarpath = os.path.join(DIST, f"vesin-single-build-v{version}.tar.gz")
         with tarfile.open(tarpath, "w:gz") as tar:
             tar.add(
-                os.path.join(HERE, "vesin-single-build.cpp"),
+                os.path.join(DIST, "vesin-single-build.cpp"),
                 arcname="vesin-single-build.cpp",
             )
             tar.add(
-                os.path.join(HERE, "vesin", "include", "vesin.h"),
+                os.path.join(DIST, "vesin-single-build-nocuda.cpp"),
+                arcname="vesin-single-build-nocuda.cpp",
+            )
+            tar.add(
+                os.path.join(DIST, "vesin.h"),
                 arcname="vesin.h",
             )
 
-    print(
-        f"created 'vesin-single-build.cpp' and 'vesin-single-build-v{version}.tar.gz'"
-    )
+    print(f"created single build files in {DIST}/")
